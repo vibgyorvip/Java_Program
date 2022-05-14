7d039d9e9804a173c9e7e20e59da727d9af244ff
vibgyorvip <86738105+vibgyorvip@users.noreply.github.com>
2022-05-14 18:43:27 +0530

    loop Basic Ex

---
+++ LoopEx.java
@@ -0,0 +1,18 @@
+
+public class LoopEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int counter=0 ; counter < 3 ; counter++ )
+		{
+			System.out.println("Hello World !! ");
+			System.out.println(counter);
+			//System.out.print(counter+" ");
+		}
+	}
+
+}
+
+
+
+
