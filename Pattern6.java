faea5ba4b15b20398cf855c8c705aabc1fb7b188
vibgyorvip <86738105+vibgyorvip@users.noreply.github.com>
2022-05-14 18:59:22 +0530

    Printing pattern6
    1 
    1 2 
    1 2 3 
    1 2 3 4

---
+++ Pattern6.java
@@ -0,0 +1,20 @@
+
+public class Pattern6 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int row=4;
+		//outer loop
+		for(int i=1; i<=row ; i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+		
+		}
+
+}
