9cf639e6b0046cf9efbf8fa000f2127f2ebcf88d
vibgyorvip <86738105+vibgyorvip@users.noreply.github.com>
2022-05-14 18:52:24 +0530

    Printing solid rectangle
    *****
    *****
    *****
    *****

---
+++ Pattern1.java
@@ -0,0 +1,18 @@
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int row=4;
+		int column=5;
+		
+		for(int i=1; i<=row ; i++) {
+			for(int j=1; j<=column;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
