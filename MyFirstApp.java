a3730d791408526222e7ece50f45ec5f6b2e5320
vibgyorvip <86738105+vibgyorvip@users.noreply.github.com>
2022-05-14 18:36:44 +0530

    Taking input using scanner class

---
+++ MyFirstApp.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class MyFirstApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		String name= sc.next(); //one token
+		//System.out.println("Hi");
+		System.out.println(name);
+		System.out.println("bye");
+		Scanner scc= new Scanner(System.in);
+		String pname= scc.nextLine(); // one complete line
+		//System.out.println("Hi");
+		System.out.println(pname);
+		System.out.println("bye");
+
+	}
+
+}
