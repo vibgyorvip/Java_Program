e205aeed650906d3016c748f4d9f488531779dc5
vibgyorvip <86738105+vibgyorvip@users.noreply.github.com>
2022-05-14 19:16:56 +0530

    taking a string from user and printing

---
+++ StringEx1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class StringEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc= new Scanner(System.in);
+		//String name=sc.next();
+		String name=sc.nextLine();
+		System.out.println(name);
+	}
+
+}
