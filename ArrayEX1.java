1b55ea2d7176928a4e18468fc606979c1fdb7957
vibgyorvip <86738105+vibgyorvip@users.noreply.github.com>
2022-05-14 19:11:13 +0530

    Basic Array Ex

---
+++ ArrayEX1.java
@@ -0,0 +1,19 @@
+
+public class ArrayEX1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//int marks[]={90,95,93};
+		//int[] marks= new int[3];
+		int marks[]=new int[3];
+		marks[0]=90;
+		marks[1]=95;
+		marks[2]=93;
+		
+		for(int i=0; i<3 ;i++)
+		{
+			System.out.println(marks[i]);
+		}
+	}
+
+}
