8debb8ca3871a71f90f625e056c5feed657f850f
vibgyorvip <86738105+vibgyorvip@users.noreply.github.com>
2022-05-14 18:39:45 +0530

    Taking 2 number as input and adding them

---
+++ Addition.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 1st number");
+		int a= sc.nextInt();
+		System.out.println("Enter 2nd number");
+		int b= sc.nextInt();
+		int c= a+b;
+		System.out.println("The sum :"+c);
+	}
+
+}
